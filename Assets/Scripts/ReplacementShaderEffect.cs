2b90b5d10ff5761f11a8854622f366b8768c19b1
diskhkme <diskhkme@gmail.com>
2020-04-20 23:36:41 +0900

    Replacement Shader 여러 객체에 동시에 다른 셰이더를 적용하는 방법. (동숲 셰이더에도 적용 가능할 듯)

---
+++ Assets/Scripts/ReplacementShaderEffect.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[ExecuteInEditMode]
+public class ReplacementShaderEffect : MonoBehaviour
+{
+    public Shader ReplacementShader;
+    public Color OverDrawColor;
+
+    void OnValidate()
+    {
+        //Material이 아닌 상태의 Shader에 값을 할당하는 방법
+        Shader.SetGlobalColor("_OverDrawColor", OverDrawColor);
+    }
+
+    void OnEnable()
+    {
+        if (ReplacementShader != null)
+            GetComponent<Camera>().SetReplacementShader(ReplacementShader, ""); //두 번째 인자가 비어있으면, 처음 있는 렌더 타입에 맞는 셰이더 사용
+    }
+
+    void OnDisable()
+    {
+        GetComponent<Camera>().ResetReplacementShader();
+    }
+}
