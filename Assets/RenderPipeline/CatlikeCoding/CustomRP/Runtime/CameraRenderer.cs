2c97b496b0003f9156b9704396aab962fca0ffcd
diskhkme <diskhkme@gmail.com>
2020-04-28 14:12:11 +0900

    Custom RP는 나중에...Object Management 먼저

--- Assets/RenderPipeline/CatlikeCoding/CustomRP/Runtime/CameraRenderer.cs
+++ Assets/RenderPipeline/CatlikeCoding/CustomRP/Runtime/CameraRenderer.cs
@@ -8,16 +8,28 @@ public class CameraRenderer
 
     Camera camera;
 
+    const string bufferName = "Render Camera";
+    CommandBuffer buffer = new CommandBuffer { name = bufferName };
+
     public void Render(ScriptableRenderContext context, Camera camera)
     {
         this.context = context;
         this.camera = camera;
+
+        Setup();
 
         //이제 실제로 그리도록 해야 함. 간단히 메소드 하나 분리
         DrawVisibleGeometry();
 
         //실제로 그려지는 것은 context queue의 작업이 submit 되었을 때
         Submit();
+    }
+
+    void Setup()
+    {
+        buffer.BeginSample(bufferName);
+        ExecuteBuffer();
+        context.SetupCameraProperties(camera); //MVP matrix등 카메라 정보를 context에 적용
     }
 
     void DrawVisibleGeometry()
@@ -27,6 +39,14 @@ public class CameraRenderer
 
     void Submit()
     {
+        buffer.EndSample(bufferName);
+        ExecuteBuffer();
         context.Submit();
+    }
+
+    void ExecuteBuffer()
+    {
+        context.ExecuteCommandBuffer(buffer);
+        buffer.Clear();
     }
 }
