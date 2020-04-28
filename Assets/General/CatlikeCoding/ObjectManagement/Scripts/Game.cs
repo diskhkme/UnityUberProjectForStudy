81913b30998c50212cf84adfd23d909a74100569
diskhkme <diskhkme@gmail.com>
2020-04-28 16:04:25 +0900

    Catlike, Object Management, Shape factory using scriptable object

--- Assets/General/CatlikeCoding/ObjectManagement/Scripts/Game.cs
+++ Assets/General/CatlikeCoding/ObjectManagement/Scripts/Game.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 //Game 클래스 자체를 persistableObject 상속하도록 바꿈!
 public class Game : PersistableObject
 {
-    public PersistableObject prefab;
+    public ShapeFactory shapeFactory;
     public PersistentStorage storage;
 
     public KeyCode createKey = KeyCode.C;
@@ -12,18 +12,18 @@ public class Game : PersistableObject
     public KeyCode saveKey = KeyCode.S;
     public KeyCode loadKey = KeyCode.L;
 
-    List<PersistableObject> objects;
+    List<Shape> shapes;
 
     private void Awake()
     {
-        objects = new List<PersistableObject>();
+        shapes = new List<Shape>();
     }
 
     private void Update()
     {
         if(Input.GetKeyDown(createKey))
         {
-            CreateObject();
+            CreateShape();
         }
         else if(Input.GetKeyDown(newGameKey))
         {
@@ -42,29 +42,29 @@ public class Game : PersistableObject
 
     private void BeginNewGame()
     {
-        for(int i=0;i<objects.Count;i++)
+        for(int i=0;i<shapes.Count;i++)
         {
-            Destroy(objects[i].gameObject);
+            Destroy(shapes[i].gameObject);
         }
-        objects.Clear();
+        shapes.Clear();
     }
 
-    void CreateObject()
+    void CreateShape()
     {
-        PersistableObject o = Instantiate(prefab);
-        Transform t = o.transform;
+        Shape instance = shapeFactory.GetRandom();
+        Transform t = instance.transform;
         t.localPosition = Random.insideUnitSphere * 5f;
         t.localRotation = Random.rotation;
         t.localScale = Vector3.one * Random.Range(0.1f, 1f);
-        objects.Add(o);
+        shapes.Add(instance);
     }
 
     public override void Save(GameDataWriter writer)
     {
-        writer.Write(objects.Count);
-        for(int i=0;i<objects.Count;i++)
+        writer.Write(shapes.Count);
+        for(int i=0;i<shapes.Count;i++)
         {
-            objects[i].Save(writer);
+            shapes[i].Save(writer);
         }
     }
 
@@ -73,9 +73,9 @@ public class Game : PersistableObject
         int count = reader.ReadInt();
         for (int i = 0; i < count; i++)
         {
-            PersistableObject o = Instantiate(prefab);
+            Shape o = shapeFactory.Get(0);
             o.Load(reader);
-            objects.Add(o);
+            shapes.Add(o);
         }
     }
 
