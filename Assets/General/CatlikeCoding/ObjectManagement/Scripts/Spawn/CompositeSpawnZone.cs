60f83abcbf7a87a76fdfb833a7e6b8fc0737dfdc
Hyungki Kim <diskhkme@gmail.com>
2020-04-30 16:48:02 +0900

    Catlike, Object Management, Zone types

---
+++ Assets/General/CatlikeCoding/ObjectManagement/Scripts/Spawn/CompositeSpawnZone.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CompositeSpawnZone : SpawnZone
+{
+    [SerializeField] SpawnZone[] spawnZones;
+    
+    public override Vector3 SpawnPoint
+    {
+        get
+        {
+            int index = Random.Range(0, spawnZones.Length);
+            return spawnZones[index].SpawnPoint;
+        }
+    }
+
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.color = Color.cyan;
+    //    Gizmos.matrix = this.transform.localToWorldMatrix;
+    //    Gizmos.DrawCube(Vector3.zero, Vector3.one);
+    //}
+
+}
