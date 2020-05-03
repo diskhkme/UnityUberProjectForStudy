5dcb80a207f28a6368da6a08415809f34e26e00f
Hyungki Kim <diskhkme@gmail.com>
2020-05-03 19:40:49 +0900

    Catlike, Tower Defense, The Board, 2. Pathfinding

--- Assets/General/CatlikeCoding/Tower Defense/Scripts/Board/GameBoard.cs
+++ Assets/General/CatlikeCoding/Tower Defense/Scripts/Board/GameBoard.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class GameBoard : MonoBehaviour
 {
@@ -6,6 +7,9 @@ public class GameBoard : MonoBehaviour
     [SerializeField] GameObject tilePrefab = default;
     Vector2Int size;
     GameTile[] tiles;
+
+    //path finding하면서 담을 정보
+    Queue<GameTile> searchFrontier = new Queue<GameTile>();
 
     public void Initialize(Vector2Int size)
     {
@@ -21,7 +25,59 @@ public class GameBoard : MonoBehaviour
                 GameTile tile = tiles[i] = Instantiate(tilePrefab).GetComponent<GameTile>();;
                 tile.transform.SetParent(this.transform, false);
                 tile.transform.localPosition = new Vector3(x - offset.x, 0f, y - offset.y);
+
+                if (x > 0)
+                    GameTile.MakeEastWestNeighbor(tile, tiles[i - 1]);
+                if (y > 0)
+                    GameTile.MakeNorthSouthNeighbors(tile, tiles[i - size.x]);
+
+                tile.IsAlternative = (x & 1) == 0; //짝수 번째는 alternative
+                if((y&1) == 0)
+                {
+                    tile.IsAlternative = !tile.IsAlternative;
+                }
             }
+        }
+
+        FindPaths();
+    }
+
+    void FindPaths()
+    {
+        foreach(GameTile tile in tiles)
+        {
+            tile.ClearPath();
+        }
+        tiles[tiles.Length/2].BecomeDestination(); //정가운데 타일을 목적지로
+        searchFrontier.Enqueue(tiles[tiles.Length/2]);
+        
+        while(searchFrontier.Count>0)
+        {
+            GameTile tile = searchFrontier.Dequeue(); //queue 타일 하나 제거
+            if(tile != null)
+            {
+                if (tile.IsAlternative)
+                {
+                    searchFrontier.Enqueue(tile.GrowPathNorth()); //그 타일의 사방 인접 정보 탐색하여 업데이트 후 큐에 삽입
+                    searchFrontier.Enqueue(tile.GrowPathSouth());
+                    searchFrontier.Enqueue(tile.GrowPathEast());
+                    searchFrontier.Enqueue(tile.GrowPathWest());
+                }
+                else
+                {
+                    searchFrontier.Enqueue(tile.GrowPathWest());
+                    searchFrontier.Enqueue(tile.GrowPathEast());
+                    searchFrontier.Enqueue(tile.GrowPathSouth());
+                    searchFrontier.Enqueue(tile.GrowPathNorth()); //그 타일의 사방 인접 정보 탐색하여 업데이트 후 큐에 삽입
+                }
+
+                
+            }
+        }
+
+        foreach(GameTile tile in tiles)
+        {
+            tile.ShowPath();
         }
     }
 }
--- Assets/General/CatlikeCoding/Tower Defense/Scripts/Board/GameTile.cs
+++ Assets/General/CatlikeCoding/Tower Defense/Scripts/Board/GameTile.cs
@@ -2,5 +2,78 @@
 
 public class GameTile : MonoBehaviour
 {
+    static Quaternion northRotation = Quaternion.Euler(90f, 0f, 0f),
+                    eastRotation = Quaternion.Euler(90f, 90f, 0f),
+                    southRotation = Quaternion.Euler(90f, 180f, 0f),
+                    westRotation = Quaternion.Euler(90f, 270f, 0f);
+
+
     [SerializeField] Transform arrow = default;
+    public bool IsAlternative { get; set; }
+
+    GameTile north, east, south, west; //각 tile은 주변 타일 ref 가지고 있음
+    GameTile nextOnPath; //갈 수 있는 다음 tile ref
+    int distance; //목적지까지 남은 거리
+
+
+    public bool HasPath => distance != int.MaxValue; //property getter
+    
+    public static void MakeEastWestNeighbor(GameTile east, GameTile west) //특정 tile이 주체가 되는 method가 아니므로 static으로 만드는 것이 명확(ex, vector3.dot / distance)
+    {
+        Debug.Assert(west.east == null && east.west == null, "Redefine neighbors!"); //한번 정하면 바뀔 수 없도록 
+        west.east = east;
+        east.west = west;
+    }
+    public static void MakeNorthSouthNeighbors(GameTile north, GameTile south)
+    {
+        Debug.Assert(south.north == null && north.south == null, "Redefined neighbors!");
+        south.north = north;
+        north.south = south;
+    }
+
+    public void ClearPath()
+    {
+        distance = int.MaxValue;
+        nextOnPath = null;
+    }
+
+    //목적지 타일 setting method
+    public void BecomeDestination()
+    {
+        distance = 0;
+        nextOnPath = null;
+    }
+
+    //현재 타일로 도달 가능한 근처 tile에 정보 삽입
+    GameTile GrowPathTo(GameTile neighbor)
+    {
+        Debug.Assert(HasPath, "No Path!");
+        if(neighbor == null || neighbor.HasPath) //null이거나, 이미 path가 설정된 경우 중단
+        {
+            return null;
+        }
+        neighbor.distance = distance + 1;
+        neighbor.nextOnPath = this;
+        return neighbor;
+    }
+
+    //각 방향으로의 growpath public methods
+    public GameTile GrowPathNorth() => GrowPathTo(north);
+    public GameTile GrowPathEast() => GrowPathTo(east);
+    public GameTile GrowPathSouth() => GrowPathTo(south);
+    public GameTile GrowPathWest() => GrowPathTo(west);
+
+    public void ShowPath()
+    {
+        if(distance == 0)
+        {
+            arrow.gameObject.SetActive(false); //목적지 tile은 deactivate
+            return;
+        }
+        arrow.gameObject.SetActive(true);
+        arrow.localRotation = nextOnPath == north ? northRotation : //nextonpath가 어느 방향에 있는지에 따라 rotation
+                            nextOnPath == east ? eastRotation :
+                            nextOnPath == south ? southRotation :
+                            westRotation;
+    }
 }
