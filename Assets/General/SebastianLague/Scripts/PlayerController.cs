4c7533d82ac590abbf7b2cdc79222e948d9593db
diskhkme <diskhkme@gmail.com>
2020-05-26 15:26:35 +0900

    Sebastian Lague, Create Game Series, 1. Player Controller

---
+++ Assets/General/SebastianLague/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    Vector3 velocity;
+    Rigidbody myRigidbody;
+    
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        myRigidbody.MovePosition(myRigidbody.position + velocity * Time.fixedDeltaTime);
+    }
+
+    public void Move(Vector3 velocity)
+    {
+        this.velocity = velocity;
+    }
+
+    public void LookAt(Vector3 lookPoint)
+    {
+        Vector3 heightCorredtedPoint = new Vector3(lookPoint.x, transform.position.y, lookPoint.z);
+        transform.LookAt(heightCorredtedPoint);
+    }
+
+
+}
