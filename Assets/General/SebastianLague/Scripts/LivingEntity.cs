c39d4f9a9221f130a7385c6813c64efe3271a138
diskhkme <diskhkme@gmail.com>
2020-05-27 14:10:52 +0900

    Sebastian Lague, Create Game Series, 5. Spawn System

--- Assets/General/SebastianLague/Scripts/LivingEntity.cs
+++ Assets/General/SebastianLague/Scripts/LivingEntity.cs
@@ -7,6 +7,9 @@ public class LivingEntity : MonoBehaviour, IDamageable
     protected float health;
     protected bool dead;
 
+    //적이 죽었을 경우, SpawnSystem이 알아야 하는데, 서로 의존성을 주는 것이 좋지 않으므로 event 사용
+    public event System.Action OnDeath;
+
     protected virtual void Start()
     {
         health = startingHealth;
@@ -25,6 +28,10 @@ public class LivingEntity : MonoBehaviour, IDamageable
     protected void Die()
     {
         dead = true;
+        if(OnDeath != null)
+        {
+            OnDeath();
+        }
         Destroy(gameObject);
     }
 }
