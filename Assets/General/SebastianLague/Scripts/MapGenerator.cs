7ab8943694163ace4229b8916d232f69efde821c
diskhkme <diskhkme@gmail.com>
2020-05-28 17:20:25 +0900

    Sebastian Lague, Create Game Series, 9. Obstacle Placement

--- Assets/General/SebastianLague/Scripts/MapGenerator.cs
+++ Assets/General/SebastianLague/Scripts/MapGenerator.cs
@@ -1,12 +1,17 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class MapGenerator : MonoBehaviour
 {
     public Transform tilePrefab;
+    public Transform obstaclePrefab;
     public Vector2 mapSize;
+    public int seed = 10;
 
-    [Range(0,1)]
-    public float outlinePercent;
+    [Range(0,1)] public float outlinePercent;
+
+    List<Coord> allTileCoords;
+    Queue<Coord> shuffledTileCoords;
 
     private void Start()
     {
@@ -15,6 +20,16 @@ public class MapGenerator : MonoBehaviour
 
     public void GenerateMap()
     {
+        allTileCoords = new List<Coord>();
+        for (int x = 0; x < mapSize.x; x++)
+        {
+            for (int y = 0; y < mapSize.y; y++)
+            {
+                allTileCoords.Add(new Coord(x, y));
+            }
+        }
+        shuffledTileCoords = new Queue<Coord>(Utility.ShuffleArray(allTileCoords.ToArray(), seed));
+
         string holderName = "Generated Map";
         if(transform.Find(holderName))
         {
@@ -29,12 +44,44 @@ public class MapGenerator : MonoBehaviour
         {
             for(int y=0;y<mapSize.y;y++)
             {
-                Vector3 tilePosition = new Vector3(-mapSize.x / 2 + 0.5f + x, 0f, -mapSize.y/2 + 0.5f + y);
+                Vector3 tilePosition = CoordToPosition(x, y);
                 Transform newTile = Instantiate(tilePrefab, tilePosition, Quaternion.Euler(Vector3.right * 90f)) as Transform;
                 newTile.localScale = Vector3.one * (1 - outlinePercent);
                 newTile.parent = mapHolder;
             }
         }
 
+        int obstacleCount = 10;
+        for(int i=0;i<obstacleCount;i++)
+        {
+            Coord randomCoord = GetRandomCoord();
+            Vector3 obstaclePosition = CoordToPosition(randomCoord.x, randomCoord.y);
+            Transform newObstacle = Instantiate(obstaclePrefab, obstaclePosition + Vector3.up*0.5f, Quaternion.identity) as Transform;
+            newObstacle.parent = mapHolder;
+        }
+
+    }
+
+    Vector3 CoordToPosition(int x, int y)
+    {
+        return new Vector3(-mapSize.x / 2 + 0.5f + x, 0f, -mapSize.y / 2 + 0.5f + y);
+    }
+
+    public Coord GetRandomCoord()
+    {
+        Coord randomCoord = shuffledTileCoords.Dequeue(); //앞에서 빼고
+        shuffledTileCoords.Enqueue(randomCoord); //뒤에 다시 집어넣음
+        return randomCoord;
+    }
+
+    public struct Coord
+    {
+        public int x;
+        public int y;
+
+        public Coord(int _x, int _y)
+        {
+            x = _x; y = _y;
+        }
     }
 }
---
+++ Assets/General/SebastianLague/Scripts/Utility.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+
+public static class Utility
+{
+    //Fisher-Yates shuffle
+    public static T[] ShuffleArray<T>(T[] array, int seed)
+    {
+        System.Random prng = new System.Random(seed);
+
+        for(int i=0;i<array.Length-1;i++) //마지막 loop는 안해도됨
+        {
+            int randomIndex = prng.Next(i, array.Length);
+            T tempItem = array[randomIndex];
+            array[randomIndex] = array[i];
+            array[i] = tempItem;
+        }
+
+        return array;
+    }
+}
