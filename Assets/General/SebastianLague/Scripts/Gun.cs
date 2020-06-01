3789af6aef1f9e5824af3a9e9962c1c6c318ac29
diskhkme <diskhkme@gmail.com>
2020-06-01 16:10:34 +0900

    Sebastian Lague, Create Game Series, 16. Weapon effects

--- Assets/General/SebastianLague/Scripts/Gun.cs
+++ Assets/General/SebastianLague/Scripts/Gun.cs
@@ -7,7 +7,16 @@ public class Gun : MonoBehaviour
     public float msBetweenShots = 100; //milisecond
     public float muzzleVelocity = 35;
 
+    public Transform shell;
+    public Transform shellEjection;
+    MuzzleFlash muzzleFlash;
+
     float nextShotTime;
+
+    private void Start()
+    {
+        muzzleFlash = GetComponent<MuzzleFlash>();
+    }
 
     public void Shoot()
     {
@@ -16,8 +25,10 @@ public class Gun : MonoBehaviour
             nextShotTime = Time.time + msBetweenShots / 1000;
             Projectile newProjectile = Instantiate(projectile, muzzle.position, muzzle.rotation) as Projectile;
             newProjectile.SetSpeed(muzzleVelocity);
+
+            Instantiate(shell, shellEjection.position, shellEjection.rotation);
+            muzzleFlash.Activate();
         }
 
-        
     }
 }
---
+++ Assets/General/SebastianLague/Scripts/Shell.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Shell : MonoBehaviour
+{
+    public Rigidbody myRigidbody;
+    public float forceMin;
+    public float forceMax;
+
+    float lifeTime = 4f;
+    float fadeTime = 2f;
+
+    private void Start()
+    {
+        float force = Random.Range(forceMin, forceMax);
+        myRigidbody.AddForce(transform.right * force);
+        myRigidbody.AddTorque(Random.insideUnitSphere * force);
+
+        StartCoroutine(Fade());
+    }
+
+    IEnumerator Fade()
+    {
+        yield return new WaitForSeconds(lifeTime);
+
+        float percent = 0f;
+        float fadeSpeed = 1 / fadeTime;
+        Material mat = GetComponent<Renderer>().material;
+        Color initialColor = mat.color;
+
+        while(percent < 1)
+        {
+            percent += Time.deltaTime * fadeSpeed;
+            mat.color = Color.Lerp(initialColor, Color.clear, percent);
+            yield return null;
+        }
+
+        Destroy(gameObject);
+    }
+}
